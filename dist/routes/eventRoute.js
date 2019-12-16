fe4a66746bdceeadc3d51c9649754a4f11eb8893
miami78 <miamilarry78@gmail.com>
2019-12-16 22:40:42 +0300

    ignore build folder

--- dist/routes/eventRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-// import EventsController from "../controllers/eventController";
-const router = express_1.Router();
-// router.get("/events", EventsController.getEvents);
-exports.default = router;
-//# sourceMappingURL=eventRoute.js.map
