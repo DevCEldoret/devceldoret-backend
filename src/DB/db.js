934387ac668d39684ce935139e69e95a7806122f
miami78 <miamilarry78@gmail.com>
2019-12-18 23:55:23 +0300

    schema change

--- src/DB/db.js
+++ src/DB/db.js
@@ -2,17 +2,17 @@ exports.getEvents = {
   id: 1,
   title: 'Community Challenge',
   description:'Join us as we celebrate a year of growth and collaboration',
-  location: {
+  location: [{
     "name": "Sirikwa Hotel",
     "street": "Oloo Street",
     "city": "Eldoret"
-  },
+  }],
   startTime: '2019-08-15 21:05:15.723336+07',
   endTime: '2019-08-15 21:05:15.723336+07',
   mediaLink: 'https://photos.app.goo.gl/323EcdkmFTzrfLd96',
-  speakers: {
+  speakers: [{
     "name": "Marvin Kweyu",
     "job title": "Django Developer",
     "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-  }
+  }]
 };
