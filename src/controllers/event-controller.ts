3e2c45c973aa996b85dc040c56ca2d32a4968c35
miami78 <miamilarry78@gmail.com>
2019-12-19 09:08:52 +0300

    get event query test

--- src/controllers/event-controller.ts
+++ src/controllers/event-controller.ts
@@ -5,10 +5,7 @@ async function getEvents(_req: any, res: any, next: any) {
   try {
     const events = await EventModel.getEvents(1, "Community Challenge", ["Sirikwa Hotel","Oloo Street","Eldoret"],"Join us as we celebrate a year of growth and collaboration", '2019-08-15 21:05:15.723336+07', '2019-08-15 21:05:15.723336+07', "https://photos.app.goo.gl/323EcdkmFTzrfLd96",  ["Marvin Kweyu","Django Developer","Lorem ipsum dolor sit amet, consectetur adipiscing elit."]);
     const data = [...events];
-    res.status(200).json({
-      status: "success",
-      data
-    });
+    res.status(200).json(data);
   } catch (error) {
     next(error);
   }
--- src/routes/event-route.ts
+++ src/routes/event-route.ts
@@ -1,7 +1,21 @@
 import { Router } from "express";
+//import { Pool } from 'pg';
 import getEvents from "../controllers/event-controller";
 
 const router = Router();
+// Added for testing purposes
+// const pool = new Pool({
+//   connectionString: "postgres://postgres:50filthyCENT!@localhost:5432/devceldoret"
+// });
+
+// const getEvents = (_request:any, response:any) => {
+//   pool.query('SELECT * FROM events ORDER BY id ASC', (error, results) => {
+//     if (error) {
+//       throw error;
+//     }
+//     response.status(200).json(results.rows);
+//   });
+// };
 
 router.get("/", getEvents);
 
