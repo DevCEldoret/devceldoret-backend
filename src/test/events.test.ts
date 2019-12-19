06e4dee3a0520cd97f6f3e78cd826ede74ee2fc2
miami78 <miamilarry78@gmail.com>
2019-12-19 10:57:15 +0300

    add event test

--- src/test/events.test.ts
+++ src/test/events.test.ts
@@ -7,40 +7,29 @@ const Database = require ("../DB/index");
 
 import app from "../server";
 
+const url = '/api/v1/';
+
 describe("DevC", () => {
-  // gets json response
-//   describe("GET events", () => {
-//     let id;
-//     let title;
-//     before(done => {
-//       Database.query(
-//         // Insert default Event into table events
-//         "INSERT INTO events (id, title) VALUES ($1, $2) RETURNING id",
-//         [event.getEvents.id, event.getEvents.title]
-//       ).then((value: { id: number,title:string; }) => {
-//         id = value.id;
-//         title = value.title;
-//         done();
-//       });
-//     });
-//     it("gets event information with the id of 1", done => {
-//       request(app)
-//         .get("/api/v1/events")
-//         .set("Content-Type", "application/json")
-//         .then(res => {
-//           const {
-//             body: {
-//               status,
-//               data: { value }
-//             }
-//           } = res;
-//           expect(res.status).to.equal(200);
-//           expect(status).to.equal("success");
-//           expect(value).to.have.property('id');
-//           expect(value).to.have.property('title');
-//           done();
-//         })
-//         .catch(error => done(error));
-//     });
-//   });
+  describe('User can view all events', () => {
+    it('GET /event', (done) => {
+        request(app)
+        .get('/api/v1/event')
+        .set('Accept', 'application/json')
+        .expect("Content-Type", /json/)
+        .then((response) => {
+          const { data,status } = response.body;
+          expect(response.status).to.equal(200);
+          //expect(data.id).to.be.a("number");
+          // expect(data.title).to.be.a("string");
+          // expect(data.description).to.be.a("string");
+          // expect(data.location).to.be.an("array");
+          // expect(data.startTime).to.be.a("string");
+          // expect(data.endTime).to.be.a("string");
+          // expect(data.mediaLink).to.be.a("string");
+          // expect(data.speakers).to.be.an("array");
+          done();
+        })
+        .catch((error: any) => done(error));
+    });
+  });
 });
