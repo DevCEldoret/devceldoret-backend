43e9203c365a240ae19a8bf56c4a936a9fbf8831
miami78 <miamilarry78@gmail.com>
2019-12-19 09:19:23 +0300

    fix getevent model

--- src/controllers/event-controller.ts
+++ src/controllers/event-controller.ts
@@ -3,7 +3,7 @@ import EventModel from "../models/events";
 
 async function getEvents(_req: any, res: any, next: any) {
   try {
-    const events = await EventModel.getEvents(1, "Community Challenge", ["Sirikwa Hotel","Oloo Street","Eldoret"],"Join us as we celebrate a year of growth and collaboration", '2019-08-15 21:05:15.723336+07', '2019-08-15 21:05:15.723336+07', "https://photos.app.goo.gl/323EcdkmFTzrfLd96",  ["Marvin Kweyu","Django Developer","Lorem ipsum dolor sit amet, consectetur adipiscing elit."]);
+    const events = await EventModel.getEvents();
     const data = [...events];
     res.status(200).json(data);
   } catch (error) {
--- src/models/events.ts
+++ src/models/events.ts
@@ -1,17 +1,10 @@
 import Database from "../db/index";
-import ErrorHandler from "../Utils/errorHandler";
 
 class EventModel {
-  static async getEvents( id:number, title:string, location:Array<string>,description:string, startTime:any, endTime:any, mediaLink:string, speakers:Array<string> ) {
-    const values = [id, title, location, description, startTime, endTime, mediaLink, speakers ];
-    const response = await Database.query('SELECT * FROM events WHERE id = $1', values).catch(
-      error => {
-        throw new ErrorHandler(error.message, 400);
-      }
-    );
-    if (!response) {
-      throw new Error("Event does not exist");
-    }
+  static async getEvents() {
+    const response = await Database.query('SELECT * FROM events ORDER BY id ASC', '',true).catch((error) => {
+      throw new Error(error.message);
+    });
     return response;
   }
 }
