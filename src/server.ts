d17b68a8b4b86081ef25600f9b9b81bf919c78e3
yegow <allanyego05@gmail.com>
2019-12-21 14:46:52 +0300

    Added a catch-all route returning 501 and an error handler

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 import router from "./routes";
 
@@ -6,25 +6,39 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// added by me for testing
-app.get("/", (request, response) => {
-  response.json({ message: "Server starts successfully!" });
+// CORS
+app.use((_req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE",
+  );
+  next();
 });
 
 // routes
 app.use("/api/v1/", router);
 
-// CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE"
-  );
-  next();
+// added by me for testing
+app.get("*", (_req, res) => {
+  res.status(501).json({
+    status: 'error',
+    error: 'Method not implemented',
+  });
 });
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(500).json({
+    status: 'error',
+    error: 'Oops! something went wrong with your request.',
+  });
+})
+
 
 app.set("port", process.env.PORT || "3000");
 app.listen(process.env.PORT || "3000", () => {
