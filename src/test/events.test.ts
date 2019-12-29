280bfcc5cf56e00e89cd5aeb4eb0b20958f75163
Fatma Ali <alifatma2019@gmail.com>
2019-12-29 13:11:28 +0300

    added circleci tests

--- src/test/events.test.ts
+++ src/test/events.test.ts
@@ -1,35 +1,21 @@
 import { expect } from "chai";
-
 import request from "supertest";
-
-const getEvents = require("../DB/db.js");
-const Database = require ("../DB/index");
-
 import app from "../server";
 
-const url = '/api/v1/';
+const baseUrl = '/api/v1/';
 
-describe("DevC", () => {
-  describe('User can view all events', () => {
-    it('GET /event', (done) => {
-        request(app)
-        .get('/api/v1/event')
-        .set('Accept', 'application/json')
-        .expect("Content-Type", /json/)
-        .then((response) => {
-          const { data,status } = response.body;
-          expect(response.status).to.equal(200);
-          //expect(data.id).to.be.a("number");
-          // expect(data.title).to.be.a("string");
-          // expect(data.description).to.be.a("string");
-          // expect(data.location).to.be.an("array");
-          // expect(data.startTime).to.be.a("string");
-          // expect(data.endTime).to.be.a("string");
-          // expect(data.mediaLink).to.be.a("string");
-          // expect(data.speakers).to.be.an("array");
-          done();
-        })
-        .catch((error: any) => done(error));
+describe("Events", () => {
+    it('GET /events', async () => {
+        const response = await request(app)
+        .get(`${baseUrl}/events`);
+        
+        const [data] = response.body;
+        console.log(data);
+        expect(data.id).to.be.a("number");
+        expect(data.name).to.be.a("string");
+        expect(data.description).not.to.be.undefined;
+        expect(data.start_datetime).to.be.a("string");
+        expect(data.end_datetime).to.be.a("string");
+        expect(data.media_link).not.to.be.undefined;   
     });
-  });
 });
