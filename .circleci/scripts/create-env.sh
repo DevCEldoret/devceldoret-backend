4e9a2ca31ad19b73fb8187ef1297902d68e3db5c
Fatma Ali <alifatma2019@gmail.com>
2019-12-29 15:12:02 +0300

    fixed password env in create-env.sh

--- .circleci/scripts/create-env.sh
+++ .circleci/scripts/create-env.sh
@@ -4,6 +4,6 @@ echo "host=${host}" >> .env
 echo "user=${user}" >> .env
 echo "type=${type}" >> .env
 echo "port=${port}" >> .env
-echo "pass=${password}" >> .env
+echo "pass=${pass}" >> .env
 cat .env
 ls
