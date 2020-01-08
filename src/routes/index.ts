07631aad73a2d89eea521d8744b799a38265ec1a
Kipruto Barno <barxwells@gmail.com>
2020-01-08 16:55:00 +0300

    Update index.ts
    delete comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,6 @@
 import { Response, Request, Router } from "express";
 import prompt from "../Utils/apiResponses";
 
-// const baseUrl = "/api/v1/";
-
-// export class TestRoute {
-//   public routes(app): void {
-//     app.route(`${baseUrl}`).get((_req: Request, res: Response) => {
-//       prompt.successMessage(res, 200, "Server successfully running");
-//     });
-//   }
-// }
 const homeRouter = Router();
 homeRouter.get("/", (req: Request, res: Response) => {
   prompt.successMessage(res, 200, "Server successfully running");
